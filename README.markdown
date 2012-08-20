a9587b74bddcd43da6a30fdb99eae86a886ee0d0
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-08-20 12:22:18 -0400

    Initial commit
    This commit adds the license, a basic README, and a program which is
    capable of fetching the JSON information for the issues of a given
    Github repository.  This gives us a good foundation to begin adding
    actual user functionality.

---
+++ README.markdown
@@ -0,0 +1,11 @@
+# save-github-issues
+
+This Perl program can automatically create a database of repository
+issues for backup purposes, since Github currently does not provide
+that functionality.
+
+
+
+# License
+
+GNU General Public License Version 3
---
+++ save-github-issues.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+#
+# save-github-issues <account> <repository>
+#
+# This program saves all of the issues for given repository owned by
+# the given account.  This provides a way to backup the issue data,
+# which is functionality that Github currently lacks (at the time of
+# writing this program).  See the file README.markdown for information
+# on how to use the program.
+#
+#
+#
+# Written by Eric James Michael Ritz
+#     <lobbyjones@gmail.com>
+#     https://github.com/ejmr/save-github-issues
+#
+################################################################################
+
+use common::sense;
+use LWP::UserAgent;
+use JSON;
+
+our $VERSION = "1.0";
+
+################################################################################
+
+# The base URI for the Github API.
+our $github_api_uri = "https://api.github.com";
+
+# Our user agent (i.e. browser) for communicating with Github.  We
+# give it a unique user agent name so that server logs over at Github
+# can recognize our program.
+our $user_agent = LWP::UserAgent->new
+    and $user_agent->agent("save-github-issues/$VERSION");
+
+################################################################################
+
+# Returns an array reference representing the issue information for
+# the given repository.  The Github user must be in the special
+# variable $_ right before we call this function.  If we cannot fetch
+# the info then the program will die with an error.
+#
+# The $_ requirement is actually to help readability.  It forces is to
+# write constructs such as
+#
+#     for ("ejmr") { get_issues_for("php-mode") }
+#
+# which read close to English.
+sub get_issues_for($) {
+    my ($repo) = @_;
+    my $user = $_;
+    my $uri = "${github_api_uri}/repos/$user/$repo/issues";
+    my $request = HTTP::Request->new(GET => $uri);
+    my $response = $user_agent->request($request);
+
+    if ($response->is_success) {
+        return decode_json($response->content);
+    }
+
+    die($response->message);
+}
+
+__END__
